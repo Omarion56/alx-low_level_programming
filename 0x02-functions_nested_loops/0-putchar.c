26011c80a69982cc961defd8bab6caa65cc06853
Omarion56 <ader20son@gmail.com>
2023-06-20 12:03:26 -0700

    kk

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+* main - execute _putchar function
+*
+* Description: _putchar function
+* this program execute _putchar function
+* Return: 0
+*/
+
+int main(void)
+{
+int _putchar(char c);
+return (0);
+}
+
